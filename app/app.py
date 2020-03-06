ac798e569c6ee068ac21d347e67496cb00e26ec2
Siyabonga Thwala <siyabongat@sahomeloans.com>
2020-03-06 11:56:57 +0200

    added kata

--- app/app.py
+++ app/app.py
@@ -1,7 +1,29 @@
-def divisors(num):
-    l = [a for a in range(2,num) if num%a == 0]
-    if len(l) == 0:
-        return str(num) + " is prime"
-    print(l)
+# def divisors(num):
+#     l = [a for a in range(2,num) if num%a == 0]
+#     if len(l) == 0:
+#         return str(num) + " is prime"
+#     print(l)
 
-divisors(20)
+# divisors(20)
+
+# def last_digit(num1,num2):
+#     exponent= pow(num1 , num2)
+#     NewList=[int(x) for x in str(exponent)]
+#     print(NewList[-1])
+
+# last_digit(10,10**10)
+
+def alphanumeric(password):
+    digits = [0,1,2,3,4,5,6,7,8,9]
+    excluded = ["",'_']
+    UpperPassWord = password.upper()
+    
+    NewPassword = list(UpperPassWord)
+
+    for item in digits:
+        if str(item) in UpperPassWord:
+            print('pass')
+        else:
+            print('fail')    
+
+alphanumeric("PassW0rd")                        
