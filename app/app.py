9911a4f090f85eaf709ac4692251ed742ed65bc1
Siyabonga Thwala <siyabongat@sahomeloans.com>
2020-03-09 14:19:36 +0200

    similar words

--- app/app.py
+++ app/app.py
@@ -15,14 +15,30 @@
 # people_in_the_bus([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]])    
 
 
-def anagrams(word,words):
-    sorted_word = sorted(word)
-    new_list = []
-    for item in words:
-        if sorted(item) == sorted_word:
-            new_list.append(item)
+# def anagrams(word,words):
+#     sorted_word = sorted(word)
+#     new_list = []
+#     for item in words:
+#         if sorted(item) == sorted_word:
+#             new_list.append(item)
 
-    print(new_list)
+#     print(new_list)
 
 
-anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])   
+# anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])   
+import difflib
+class Dictionary:
+    def __init__(self,words):
+        self.words = words
+
+    def find_most_similar(self, term): 
+        best_match = difflib.get_close_matches(term,self.words,1)
+        if best_match == []:
+            best_match.append('zqdrhpviqslik')
+           
+        print(best_match[0])
+       
+
+
+words=Dictionary(['javascript', 'java', 'ruby', 'php', 'python', 'coffeescript'])
+words.find_most_similar('heaven')
